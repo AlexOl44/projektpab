17f5ac12a4aa2e948b4213a0c7f762f7d0488ea3
AlexOl44 <230746@stud.usz.edu.pl>
2025-01-17 18:51:54 +0100

    commit proj

--- src/main/java/com/example/demo/controller/CategoryController.java
+++ src/main/java/com/example/demo/controller/CategoryController.java
@@ -1,8 +1,7 @@
 package com.example.demo.controller;
 
-//ELELELELELEOEOEO
-
 import com.example.demo.model.Category;
+import com.example.demo.model.Expense;  // Importuj klasę Expense
 import com.example.demo.service.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -12,12 +11,15 @@ import org.springframework.web.bind.annotation.*;
 @Controller
 @RequestMapping("/categories")
 public class CategoryController {
+
     @Autowired
     private CategoryService categoryService;
 
     @GetMapping
     public String showAllCategories(Model model) {
         model.addAttribute("categories", categoryService.findAll());
+        model.addAttribute("category", new Category());  // Dodaj obiekt category
+        model.addAttribute("expense", new Expense());  // Dodaj obiekt expense
         return "categories/list";
     }
 
@@ -33,3 +35,4 @@ public class CategoryController {
         return "redirect:/categories";
     }
 }
+
